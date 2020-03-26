e198e7278ae940268b744dce3f0f90110258093d
tsuka414 <keita04141537@icloud.com>
2020-03-26 06:26:19 +0000

    Finish page-layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   # ページごとにタイトルを返す
-  def full_title(page_name = "")　#メソッドと引数の定義
+  def full_title(page_name = "")
     base_title = "AttendanceApp"
     if page_name.empty?
       base_title
